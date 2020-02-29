57b35ccfeaa3f62626f5fd12cd1b52a1a88ffac5
s10926 <s10926@pjwstk.edu.pl>
2020-02-29 21:23:08 +0100

    Druga zmiana

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                //Console.WriteLine(html);
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(html);
+
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
